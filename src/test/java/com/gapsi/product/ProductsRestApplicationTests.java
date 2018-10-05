6a9cfe316f94d2781496c09c3854838e15360567
Jose Bello <jluisbn89@gmail.com>
2018-10-05 17:58:36 -0500

    adding unit test

--- src/test/java/com/gapsi/product/ProductsRestApplicationTests.java
+++ src/test/java/com/gapsi/product/ProductsRestApplicationTests.java
@@ -1,16 +1,34 @@
 package com.gapsi.product;
 
+import static org.junit.Assert.assertTrue;
+
+import java.sql.SQLException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.gapsi.product.model.ProductModel;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProductsRestApplicationTests {
 
+	private ProductModel pm = new ProductModel();
+	
 	@Test
-	public void contextLoads() {
+	public void testAddProduct() throws SQLException 
+	{
+		int res = pm.addProduct("nombre", "descripcion", "20", "modelo", "1");
+		assertTrue(res > 0);
+	}
+	
+	@Test
+	public void testUpdateProduct() throws SQLException 
+	{
+		int res = pm.updateProduct(1, "descripcion", "modelo");
+		assertTrue(res > 0);
 	}
 
 }
